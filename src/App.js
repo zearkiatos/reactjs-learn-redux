ca10d0866d01c095943932da304fcbcc8cd17bbb
zearkiatos <caprilespe@outlook.com>
2021-03-20 12:03:06 -0300

    Modeling with Redux

--- src/App.js
+++ src/App.js
@@ -1,4 +1,36 @@
 import React from 'react';
+import { } from 'redux'
+
+// People Dropping off a form (action creators)
+
+const createPolicy = (name, amount) => {
+    return { // Action (a form in our analogy)
+       type: 'CREATE_POLICY',
+       payload: {
+           name,
+           amount
+       }
+    }
+};
+
+const deletePolicy = (name) => {
+    return {
+        type: 'DELETE_POLICY',
+        payload: {
+            name
+        }
+    }
+};
+
+const createClaim = (name, amountOfMoneyToCollect) => {
+    return {
+        type: 'CREATE_CLAIM',
+        payload: {
+            name,
+            amountOfMoneyToCollect
+        }
+    }
+}
 
 export default () => {
     return (
